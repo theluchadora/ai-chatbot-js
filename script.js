9ce229f80af4f135415a47bdbd4264390c87afff
theluchadora <160585859+theluchadora@users.noreply.github.com>
2025-05-05 23:24:02 +0300

    removing some part

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const fileInput = document.querySelector("#file-input");
 const fileUploadWrapper = document.querySelector(".file-upload-wrapper");
 const fileCancelButton = document.querySelector("#file-cancel");
 
-const API_KEY = "AIzaSyBx3w_q0_DAY4fwbvBwBcYdGgXk2oVavSI";
+const API_KEY = "Your api ";
 const API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${API_KEY}`;
 
 const userData = {
